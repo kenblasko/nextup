4cddc74b8fa383cbdd3ee57de2096a52db6010be
Ken Blasko <ken@gorocketfuel.com>
2017-06-11 15:51:30 -0500

    hook up carousel and modals

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,7 +5,13 @@ $(() => {
     if ($.Redactor) {
         $('textarea').redactor()
     }
+    $('.carousel').carousel({
+        indicators: true,
+        noWrap: true
+        // fullWidth: true
+    });
     $(".button-collapse").sideNav()
+    $('.modal').modal();
 });
 
 export default site;

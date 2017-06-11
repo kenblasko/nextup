51f496866b537e260b44b86fbdc8257588a29345
Ken Blasko <ken@gorocketfuel.com>
2017-06-11 15:50:53 -0500

    add db seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,30 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Nextup.Repo.insert!(%Nextup.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Nextup.DatabaseSeeder do
+  alias Nextup.Repo
+  alias Nextup.Sets.Card
+
+  @titles_list [
+    "Warrior", 
+    "Rogue", 
+    "Sorcerer"
+  ]
+  @content_list [
+    "Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.", 
+    "Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Curabitur blandit tempus porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.", 
+    "Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
+  ]
+
+  def insert_card do
+    Repo.insert! %Card{
+      title: @titles_list |> Enum.take_random(1) |> hd,
+      body: @content_list |> Enum.take_random(1) |> hd,
+      user_id: 1
+    }
+  end
+
+  def clear do
+    Repo.delete_all
+  end
+
+end
+
+(1..20) |> Enum.each(fn _ -> Nextup.DatabaseSeeder.insert_card end)

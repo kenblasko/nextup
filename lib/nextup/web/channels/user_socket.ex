0d0d36fe4d9271f3abd862e3822e0b8d1b123969
Ken Blasko <kjb820@gmail.com>
2017-06-12 13:38:15 -0500

    setup prod config

--- lib/nextup/web/channels/user_socket.ex
+++ lib/nextup/web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Nextup.Web.UserSocket do
   # channel "room:*", Nextup.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can

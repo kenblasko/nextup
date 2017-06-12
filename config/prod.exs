1de5ae5f941165d7934254f5647ad108778ea926
Ken Blasko <kjb820@gmail.com>
2017-06-12 13:45:29 -0500

    fix syntax err

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :nextup, Nextup.Web.Endpoint,
 config :logger, level: :info
 
 # Configure databse
-config :nextup Nextup.Repo,
+config :nextup, Nextup.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),

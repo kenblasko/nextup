6d1c8033a285b3652dabf24a474ce5b96f5fc5bd
Ken Blasko <kjb820@gmail.com>
2017-06-13 07:29:40 -0500

    generated test

--- test/nextup/sets/sets_test.exs
+++ test/nextup/sets/sets_test.exs
@@ -242,4 +242,66 @@ defmodule Nextup.SetsTest do
       assert %Ecto.Changeset{} = Sets.change_card(card)
     end
   end
+
+  describe "groups" do
+    alias Nextup.Sets.Group
+
+    @valid_attrs %{body: "some body", title: "some title"}
+    @update_attrs %{body: "some updated body", title: "some updated title"}
+    @invalid_attrs %{body: nil, title: nil}
+
+    def group_fixture(attrs \\ %{}) do
+      {:ok, group} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Sets.create_group()
+
+      group
+    end
+
+    test "list_groups/0 returns all groups" do
+      group = group_fixture()
+      assert Sets.list_groups() == [group]
+    end
+
+    test "get_group!/1 returns the group with given id" do
+      group = group_fixture()
+      assert Sets.get_group!(group.id) == group
+    end
+
+    test "create_group/1 with valid data creates a group" do
+      assert {:ok, %Group{} = group} = Sets.create_group(@valid_attrs)
+      assert group.body == "some body"
+      assert group.title == "some title"
+    end
+
+    test "create_group/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Sets.create_group(@invalid_attrs)
+    end
+
+    test "update_group/2 with valid data updates the group" do
+      group = group_fixture()
+      assert {:ok, group} = Sets.update_group(group, @update_attrs)
+      assert %Group{} = group
+      assert group.body == "some updated body"
+      assert group.title == "some updated title"
+    end
+
+    test "update_group/2 with invalid data returns error changeset" do
+      group = group_fixture()
+      assert {:error, %Ecto.Changeset{}} = Sets.update_group(group, @invalid_attrs)
+      assert group == Sets.get_group!(group.id)
+    end
+
+    test "delete_group/1 deletes the group" do
+      group = group_fixture()
+      assert {:ok, %Group{}} = Sets.delete_group(group)
+      assert_raise Ecto.NoResultsError, fn -> Sets.get_group!(group.id) end
+    end
+
+    test "change_group/1 returns a group changeset" do
+      group = group_fixture()
+      assert %Ecto.Changeset{} = Sets.change_group(group)
+    end
+  end
 end

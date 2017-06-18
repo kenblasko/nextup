6afb0bfd05f9be1b2ad9e5e21cc8710d22c96b2f
Ken Blasko <kjb820@gmail.com>
2017-06-17 19:05:26 -0500

    refactor sortable for simple drag and drop

--- assets/js/sortable.js
+++ assets/js/sortable.js
@@ -1,54 +1,31 @@
 let sortable
 let root = document.querySelector('.sortable')
 let existingRoot = document.querySelector('.sortable-existing')
+let selector = '#set_ids'
+let groupName = 'sets'
 
 if (window.Sortable) {
     if (root) {
         Sortable.create(root, {
             handle: '.sortable-cursor',
-            animation: 150,
-            group: 'cards'
+            group: groupName,
+            sort: false,
         })
     }
     if (existingRoot) {
-        function removeOrderEl($el) {
-            $el.children(".sortable-order").text('')
-        }
-        function setOrder($el, idx) {
-            $el.children(".sortable-order").text(parseInt(idx) + 1)
-        }
-        function resetInputs($el, disabled = false) {
-            $el.children("#set_cards_id").prop('disabled', disabled)
-        }
-        function orderCards(e, from = false) {
-            let list = from ? e.from.children : e.to.children
-            if (list.length > 0) {
-                Array.from(list).forEach((item, idx) => {
-                    const $item = $(item)
-                    setOrder($item, idx)
-                })
-            }
-        }
         Sortable.create(existingRoot, {
             handle: '.sortable-cursor',
-            animation: 150,
-            group: 'cards',
+            group: groupName,
+            sort: false,
+            
             onAdd(e) {
-                // Make sure inputs are enabled then sort via index
-                const $item = $(e.item)
-                resetInputs($item)
-                orderCards(e)
+                // Make sure inputs are enabled
+                $(e.item).children(selector).prop('disabled', false)
             },
             onRemove(e) {
-                // Make sure inputs are disabled then sort via index
-                const $item = $(e.item)
-                resetInputs($item, true)
-                orderCards(e, true) // from will be this list
-                removeOrderEl($item)
+                // Make sure inputs are disabled
+                $(e.item).children(selector).prop('disabled', true)
             },
-            onUpdate(e) {
-                orderCards(e)
-            }
         })
     }
 }
